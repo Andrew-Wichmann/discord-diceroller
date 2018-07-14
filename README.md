3c577adf7076e32d97fc4e44742635884f5ee27a
Walter <verburg.walt@alumni.uwlax.edu>
2018-07-14 00:55:13 +0000

    Initial Commit - Uploading dicebot.py and updated README.md and .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # imperial-dicebot
 A Discord.py die rolling bot for Imperial Dawn, an excellent tabletop RPG
+The game itself is available for free at https://imperialdawn.com and it's pretty good.
+This uses the discord.py library available at https://github.com/Rapptz/discord.py
+It's pretty good, and as you can see, quite easy to set up for simple bots.
+
+When making a discord bot, be sure that you are trying to log in with your bot's token, not their Client ID or Client secret. You can do so with this code by creating a text file called "token" (not "token.txt", "token.docx", or "token.pdf", just "token") in the same directory as the code, and pasting your token into it without leaving anything in the file.
+
+This code should be considered free to use or learn from by anyone, attribution is preferred if appropriate but not required. There's probably an official license that says that, but I don't feel like digging through contracts right now, and realistically nobody's gonna rip off a bot this simple. I probably wouldn't even care if they did.
---
+++ dicebot.py
@@ -0,0 +1,83 @@
+import discord
+import asyncio
+import random
+import re
+
+#Load account token from other file
+token = open("token", "r").read().strip()
+
+
+#Global Initializations
+client = discord.Client()
+#Note that any roll with more than 300ish dice is going to fail to send anyway
+check_regex = re.compile("^[rcy](\d{1,3})(.*)", re.IGNORECASE)
+save_regex = re.compile("^[ns](\d{1,3})(.*)", re.IGNORECASE)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('--------')
+
+@client.event
+async def on_message(message):
+    print("{}:{}{}:{}".format(
+        message.author,
+        message.server.name+":" if message.server else "",
+        message.channel, 
+        message.content,
+    ))
+    #roll a check (success on 3-6)
+    if check_regex.match(message.content):
+        m = check_regex.match(message.content)
+        results = ""
+        score = 0
+        for i in range(int(m.group(1))):
+            die = random.randrange(1, 7)
+            if die > 2:
+                score += 1
+                results += " **[{}]**".format(die)
+            else:
+                results += "[{}]".format(die)
+        subject = m.group(2)
+        subject = " re: " + subject if subject else ""
+        await client.send_message(message.channel,
+                "**{0}** got {1} successes {2} on their **Check**{3}".format(message.author.display_name, score, results, subject))
+    #roll a save (success on 4-6)
+    elif save_regex.match(message.content):
+        m = save_regex.match(message.content)
+        results = ""
+        score = 0
+        for i in range(int(m.group(1))):
+            die = random.randrange(1, 7)
+            if die > 3:
+                score += 1
+                results += " **[{}]**".format(die)
+            else:
+                results += "[{}]".format(die)
+        
+        subject = m.group(2)
+        subject = " re: " + subject if subject else ""
+        await client.send_message(message.channel,
+                "**{0}** got {1} successes {2} on their **Save**{3}".format(message.author.display_name, score, results, subject))
+    #Mindless trolling
+    elif "now" in message.content.lower():
+        await slow_talk(
+            message,
+            "Now, don't be hasty young @{}.".format(message.author.mention)
+        )
+
+#not currently used but I wanted AW to see a fun thing.
+async def slow_talk(message, response):
+    msg = await client.send_message(message.channel, "hmmmmmmmmm...")
+    await asyncio.sleep(5)
+    for i in range(len(response)+1):
+        await client.edit_message(msg, response[:i])
+        await asyncio.sleep(2)
+
+
+
+#Actually kicks things off
+client.run(token)
